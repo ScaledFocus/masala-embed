b9dab34c2b935ccf47a82cfedb8e3f3d61d23257
Luv Suneja <luvsuneja@gmail.com>
2025-09-17 15:52:42 +0400

    create supabase database

---
+++ esci-dataset/database/001-create-labeler-table.sql
@@ -0,0 +1,7 @@
+-- SQL script to create 'labeler' table in Supabase
+create table labeler (
+    id serial primary key,
+    name text not null,
+    role text not null check (role in ('labeler', 'supervisor')),
+    created_at timestamp with time zone default now()
+);
---
+++ esci-dataset/database/002-insert-labeler-info.sql
@@ -0,0 +1,3 @@
+INSERT INTO labeler (name, role) VALUES
+    ('Luv', 'labeler'),
+    ('Nirant', 'labeler');
