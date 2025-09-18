42cdfee90e1ff93d27fa7bb039c1a680fddd2424
Luv Suneja <luvsuneja@gmail.com>
2025-09-18 10:17:04 +0400

    Adding git hash for ai labeler

--- esci-dataset/database/002-insert-labeler-info.sql
+++ esci-dataset/database/002-insert-labeler-info.sql
@@ -1,4 +1,4 @@
 INSERT INTO labeler (name, role, type) VALUES
     ('Luv', 'labeler', 'human'),
     ('Nirant', 'labeler', 'human'),
-    ('Synthetic Generator', 'labeler', 'synthetic generator');
+    ('4784ad5', 'labeler', 'synthetic generator');
