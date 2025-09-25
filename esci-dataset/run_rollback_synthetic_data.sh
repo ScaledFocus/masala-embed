f1db8e7a4b93a9c071e624828a366d7db1b49f03
Luv Suneja <luvsuneja@gmail.com>
2025-09-25 22:24:47 +0400

    scripts

--- esci-dataset/run_rollback_synthetic_data.sh
+++ esci-dataset/run_rollback_synthetic_data.sh
@@ -4,9 +4,9 @@
 # Edit the defaults below, then run: ./run_rollback_synthetic_data.sh
 
 # Default parameters (edit these as needed)
-RUN_ID=""
-DRY_RUN="true"
-CONFIRM=""
+RUN_ID="aefc21fcce0440a28943f874c04dd37d"
+DRY_RUN=""
+CONFIRM="true"
 
 uv run python database/scripts/rollback_synthetic_data.py \
 	$(if [ -n "$RUN_ID" ]; then echo "--run-id $RUN_ID"; fi) \
